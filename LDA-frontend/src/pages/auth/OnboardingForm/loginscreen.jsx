f5a3e544a8cfab58838a31530040a841456d8db6
Sagar6250 <sagars6250@gmail.com>
2024-03-11 17:11:23 +0530

    Changed file name

--- LDA-frontend/src/pages/auth/OnboardingForm/loginscreen.jsx
+++ LDA-frontend/src/pages/auth/OnboardingForm/loginscreen.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import RootContainer from "../../../components/layout/RootContainer";
+import { Box } from "@mui/material";
 
 const LoginScreen = () => {
-    return <RootContainer></RootContainer>;
+    return <RootContainer>
+        <Box></Box>
+    </RootContainer>;
 };
 
 export default LoginScreen;
